0a75f5ed90b8f7730257f1555c20ac06304336c4
AquaApple <yassershehab9@gmail.com>
2022-08-29 13:15:26 +0200

    Added Context to Cart-items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,18 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 
 function HeaderCartButton({ onClick }) {
+  const value = useContext(CartContext);
+
   return (
     <button className={classes.button} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{value.items.length}</span>
     </button>
   );
 }
