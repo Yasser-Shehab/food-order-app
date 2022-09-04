906e7e2b6f80a0b4b3b9997f36f8f9e34ccdc43b
AquaApple <yassershehab9@gmail.com>
2022-09-04 20:10:04 +0200

    Added IsLoading and Error to http request

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./Checkout.module.css";
+
+function Checkout() {
+  return (
+    <form>
+      <div className={classes.control}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal" />
+      </div>
+    </form>
+  );
+}
+
+export default Checkout;
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,11 +5,21 @@ import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card";
 function AvailableMeals() {
   const [meals, setMeal] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     const fetchMeals = async () => {
+      setIsLoading(true);
+
       const response = await fetch(
         "https://react-http-c6eb2-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Somthing went wrong!");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -23,11 +33,25 @@ function AvailableMeals() {
         });
       }
       setMeal(loadedMeals);
+      setIsLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setError(error.message);
+    });
   }, []);
 
+  if (isLoading) {
+    return <section className={classes.MealsLoading}>Loading...</section>;
+  }
+
+  if (error) {
+    return <section className={classes.MealsError}>{error}</section>;
+  }
+
   const mealsList = meals.map((meal) => <MealItem key={meal.id} {...meal} />);
+
   return (
     <section className={classes.meals}>
       <Card>
